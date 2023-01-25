98c36202d406e89069c4de82e9e7bdd5aed6d9b6
Hilora Dewett <h.dewett@outlook.com>
2023-01-24 19:17:06 -0500

    Add one-to-many associations and add more employees.

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Hilora", last_name: "Dewett", hourly_rate: 15)
+@store1.employees.create(first_name: "Custard", last_name: "Dewett", hourly_rate: 60)
+@store1.employees.create(first_name: "Skipper", last_name: "Dewett", hourly_rate: 2)
+
+@store2.employees.create(first_name: "Elongated", last_name: "Muskrat", hourly_rate: 100)
+@store2.employees.create(first_name: "Beff", last_name: "Jezos", hourly_rate: 90)
+@store2.employees.create(first_name: "Walen", last_name: "Geston", hourly_rate: 70)
